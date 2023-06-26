1d6fa30c86bc9a2022a7ceb1970c9672dad34d8d
lakhyun.kim <java2114@genexon.co.kr>
2023-06-26 15:37:42 +0900

    [기능추가] 스프링 부트 신규 세팅

---
+++ src/main/java/com/lh/lhb/LhbApplication.java
@@ -0,0 +1,13 @@
+package com.lh.lhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lh/lhb/LhbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.lhb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
